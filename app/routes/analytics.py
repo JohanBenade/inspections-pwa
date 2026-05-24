2ec091c4e79bbf9834a513a992de1364655ce133
JohanBenade <johan@monograph-architects.com>
2026-05-24 12:54:46 +0200

    v331a: cert_zone_grid data structure (v330 union per block/floor)

--- app/routes/analytics.py
+++ app/routes/analytics.py
@@ -6052,6 +6052,55 @@ def _build_pipeline_report_data(live=False):
         'project_avg': project_avg,
     }
 
+    # v331: cert_zone_grid - certified units per (block, floor)
+    # Reuses v330 union: _formal_certified_ids | _handover_ready_ids
+    _certified_ids = _formal_certified_ids | _handover_ready_ids
+    cert_zone_map = {}
+    for _u in all_units:
+        _key = (_u['block'], _u['floor'])
+        if _key not in cert_zone_map:
+            cert_zone_map[_key] = {
+                'total_units': 0,
+                'certified_count': 0,
+                'certified_unit_numbers': [],
+            }
+        cert_zone_map[_key]['total_units'] += 1
+        if _u['id'] in _certified_ids:
+            cert_zone_map[_key]['certified_count'] += 1
+            cert_zone_map[_key]['certified_unit_numbers'].append(_u['unit_number'])
+
+    def _collapse_unit_ranges(unit_numbers):
+        if not unit_numbers:
+            return ''
+        nums = sorted(int(n) for n in unit_numbers)
+        parts, start, prev = [], nums[0], nums[0]
+        for n in nums[1:]:
+            if n == prev + 1:
+                prev = n
+            else:
+                parts.append('{:03d}'.format(start) if start == prev else '{:03d}-{:03d}'.format(start, prev))
+                start = prev = n
+        parts.append('{:03d}'.format(start) if start == prev else '{:03d}-{:03d}'.format(start, prev))
+        return ', '.join(parts)
+
+    for _cell in cert_zone_map.values():
+        _n = _cell['total_units']
+        _x = _cell['certified_count']
+        _cell['certified_pct'] = round(100 * _x / _n, 1) if _n > 0 else 0
+        _cell['certified_units_display'] = _collapse_unit_ranges(_cell['certified_unit_numbers'])
+        if _x == 0:
+            _cell['cert_stage'] = 'none'
+        elif _x == _n:
+            _cell['cert_stage'] = 'all'
+        else:
+            _cell['cert_stage'] = 'partial'
+
+    cert_zone_grid = {
+        'blocks': all_blocks,
+        'floors': all_floors,
+        'data': cert_zone_map,
+    }
+
     # Floor label helper
     floor_labels = {0: 'Ground', 1: '1st Floor', 2: '2nd Floor'}
 
@@ -6444,6 +6493,7 @@ def _build_pipeline_report_data(live=False):
         'chart_w': chart_w,
         'chart_h': chart_h,
         'zone_grid': zone_grid,
+        'cert_zone_grid': cert_zone_grid,
         'floor_labels': floor_labels,
         'areas': areas,
         'trades': trades,
